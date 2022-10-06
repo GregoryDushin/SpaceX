4c75d334f913d5cf2d6d571f01a93e3ca2cab379
GregoryDushin <86513789+GregoryDushin@users.noreply.github.com>
2022-10-06 13:43:50 +0400

    URL Beginner

--- SpaceX/ViewController.swift
+++ SpaceX/ViewController.swift
@@ -11,9 +11,32 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        rocketDataLoad()
     }
 
+     
+    func rocketDataLoad(){
+        let session = URLSession.shared
 
+        let url = URL(string: "https://api.spacexdata.com/v4/rockets")!
+        let task = session.dataTask(with: url) { (data, responce, error) in
+            DispatchQueue.main.async{
+            guard error == nil else{
+                print("Error")
+                return
+        }
+        do {
+            let json = try JSONDecoder().decode(RocketModel.self, from: data!)
+
+            print (json[0].diameter.meters)
+
+        }catch{
+            print("wrong url")
+        }
+    }
+
+}
+        task.resume()
+}
 }
 
