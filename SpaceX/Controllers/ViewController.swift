4fc73b3feac0258629846444d3db67f4785ed1c7
GregoryDushin <86513789+GregoryDushin@users.noreply.github.com>
2022-10-07 16:48:19 +0400

    Implement gitIgnore + UIPageControl + Pics

--- SpaceX/Controllers/ViewController.swift
+++ SpaceX/Controllers/ViewController.swift
@@ -7,13 +7,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
+    
+    @IBOutlet weak var testLabel: UILabel!
+    @IBOutlet weak var pageControll: UIPageControl!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    var images: [String] = ["0","1","2"]
+    var frame = CGRect(x: 0, y: 0, width: 0, height: 0)
+    
     
     //MARK: Arrays for recieving data from parsing loaders
     
     var rockets: [RocketModelElement] = []
     var launches: [LaunchModelElement] = []
-    
 
     
     override func viewDidLoad() {
@@ -25,6 +32,8 @@ class ViewController: UIViewController {
         RocketLoader().rocketDataLoad { rockets in
             self.rockets = rockets
             print("test \(rockets.count)")
+            print(rockets[0].flickrImages.count)
+            self.testLabel.text = rockets[0].name
             
         }
         
@@ -33,7 +42,26 @@ class ViewController: UIViewController {
             print("test \(launches.count)")
         }
         
+        //MARK: - PageControll
+        
+        pageControll.numberOfPages = images.count
+        for index in 0..<images.count {
+            frame.origin.x = scrollView.frame.size.width * CGFloat(index)
+            frame.size = scrollView.frame.size
+            
+            let imageView = UIImageView(frame: frame)
+            imageView.image = UIImage(named: images[index])
+            self.scrollView.addSubview(imageView)
+        }
+        scrollView.contentSize = CGSize(width: (scrollView.frame.size.width * CGFloat(images.count)), height: scrollView.frame.size.height)
+        scrollView.delegate = self
     }
-   
+    //MARK: - ScrollView method
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        var pageNumber = scrollView.contentOffset.x / scrollView.frame.size.width
+        pageControll.currentPage = Int(pageNumber)
+    }
+
 }
 
